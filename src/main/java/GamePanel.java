6efc2381455b570c520cf981d780f938d04495ff
sigzal1105 <sigzal1105@skola.goteborg.se>
2024-01-31 18:59:32 +0100

    Added drawGround() method, javadoc, minor code fixes

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -1,14 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel implements Runnable{
+public class GamePanel extends JPanel implements Runnable {
     private final int originalTileSize = 16; // 16x16 tile
     private final int scale = 3;
+
     private final int tileSize = originalTileSize * scale; // Size of the tiles scaled, 48x48
     private final int maxScreenColumn = 10;
     private final int getMaxScreenRow = 14;
     private final int screenWidth = tileSize * maxScreenColumn; // 160 pixels
     private final int screenHeight = tileSize * getMaxScreenRow; // 224 pixels
+
+    private int scrollPosition = 0;
+    private final int scrollSpeed = 4;
 
     private int birbX = (screenWidth / 2 - 50); // x-cordinate for birb
     private int birbY = (screenHeight / 2 - 50); // y-cordinate for birb
@@ -18,9 +22,8 @@ public class GamePanel extends JPanel implements Runnable{
     private int playerHeight = 15; // birb hight
 
 
-    private Image backgroundImage; 
+    private Image backgroundImage;
     private Image groundImage;
-
 
 
     KeyControls keyControls = new KeyControls();
@@ -28,6 +31,11 @@ public class GamePanel extends JPanel implements Runnable{
     //Frames Per Second
     private final int FPS = 60;
     Thread gameThread;
+
+    public void startGame() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -44,8 +52,8 @@ public class GamePanel extends JPanel implements Runnable{
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(backgroundImage, 0, 0, screenWidth, screenHeight, this);
-        g.drawImage(groundImage, 0, 500, screenWidth, 70, this); // y for placement in height, 70 for image height
         drawPlayer(g);
+        drawGround(g);
     }
 
 
@@ -59,6 +67,26 @@ public class GamePanel extends JPanel implements Runnable{
 
 
     /**
+     * @param g
+     * This method loops the ground tiles.
+     */
+    private void drawGround(Graphics g) {
+
+        for (int i = 0; i < 20; i++) {
+            int x = i * tileSize - scrollPosition % tileSize;
+
+            g.drawImage(groundImage, x, 500, screenWidth, 70, this);
+            // y for placement in height, 70 for image height
+
+            //x = 0
+            //y = 500
+        }
+
+
+    }
+
+
+    /**
      * update() is to be put in the run() method.
      * This methods contains the controls to the birb.
      */
@@ -68,7 +96,7 @@ public class GamePanel extends JPanel implements Runnable{
 
         } else {
 
-            birbY = birbY - Integer.MAX_VALUE;
+            birbY = birbY + 3;
         }
     }
 
@@ -76,24 +104,27 @@ public class GamePanel extends JPanel implements Runnable{
     public void run() {
 
         //Tell the system when to draw the screen again.
-        double drawInterval = 1000000000/FPS; //0.016666 seconds
+        double drawInterval = (double) 1000000000 / FPS; //0.016666 seconds
         double nextDrawTime = System.nanoTime() + drawInterval;
 
         while (gameThread != null) {
             update();
+            scrollPosition = scrollPosition + scrollSpeed;
             repaint();
 
             try {
 
                 double remainingTime = nextDrawTime - System.nanoTime();
-                remainingTime = remainingTime/1000000;
+                remainingTime = remainingTime / 1000000;
 
                 //If game doesn't have any
-                if(remainingTime < 0){
+                if (remainingTime < 0) {
                     remainingTime = 0;
                 }
 
                 Thread.sleep((long) remainingTime);
+                nextDrawTime = nextDrawTime + drawInterval;
+
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
