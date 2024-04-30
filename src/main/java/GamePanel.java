aeecb9aa3bf59a2643b74c00eecf5d661786cdf9
ThereseFrimanson <thefri0827@skola.goteborg.se>
2024-04-30 09:27:48 +0200

    Adding backgorund pictures

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -386,7 +386,7 @@ public class GamePanel extends JPanel implements Runnable {
                 default -> System.err.println("Illegitimate choice");
             }
         }
-        if (panelScore >= 1305) {
+        if (panelScore >= 1305 && panelScore < 1650) {
             backgroundImage = new ImageIcon("Images/Background_sand.png").getImage();
             groundImage = new ImageIcon("Images/ground_sand2.png").getImage();
             bottomObstacle = new ImageIcon("Images/Obsticle_start_bottom.png").getImage();
@@ -399,22 +399,33 @@ public class GamePanel extends JPanel implements Runnable {
                 default -> System.err.println("Illegitimate choice");
             }
         }
-        /*
-         * if (panelScore >= 1650) {
-         * backgroundImage = new ImageIcon("Images/Desert.png").getImage();
-         * groundImage = new ImageIcon("Images/ground_sand2.png").getImage();
-         * bottomObstacle = new
-         * ImageIcon("Images/Obsticle_start_bottom.png").getImage();
-         * topObstacle = new ImageIcon("Images/Obsticle_start_top_kiwi.png").getImage();
-         * 
-         * switch (spaceBetweenObstacles) {
-         * case 170 -> scrollSpeed = 7;
-         * case 185 -> scrollSpeed = 6;
-         * case 200 -> scrollSpeed = 5;
-         * default -> System.err.println("Illegitimate choice");
-         * }
-         * }
-         */
+
+        if (panelScore >= 1650 && panelScore < 2000) {
+            backgroundImage = new ImageIcon("Images/Background_beach.png").getImage();
+            groundImage = new ImageIcon("Images/ground_sand2.png").getImage();
+            bottomObstacle = new ImageIcon("Images/Obsticle_start_bottom.png").getImage();
+            topObstacle = new ImageIcon("Images/Obsticle_start_top_kiwi.png").getImage();
+
+            switch (spaceBetweenObstacles) {
+                case 170 -> scrollSpeed = 7;
+                case 185 -> scrollSpeed = 6;
+                case 200 -> scrollSpeed = 5;
+                default -> System.err.println("Illegitimate choice");
+            }
+        }
+        if (panelScore >= 2000) {
+            backgroundImage = new ImageIcon("Images/Background_snow.png").getImage();
+            groundImage = new ImageIcon("Images/Ground_snow.png").getImage();
+            bottomObstacle = new ImageIcon("Images/Obsticle_start_bottom.png").getImage();
+            topObstacle = new ImageIcon("Images/Obsticle_start_top_kiwi.png").getImage();
+
+            switch (spaceBetweenObstacles) {
+                case 170 -> scrollSpeed = 7;
+                case 185 -> scrollSpeed = 6;
+                case 200 -> scrollSpeed = 5;
+                default -> System.err.println("Illegitimate choice");
+            }
+        }
     }
 
     /**
